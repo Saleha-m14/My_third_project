fc7329aedb1a92899a63532d9eb3c7fae0a9394f
Saleha Mohammadi <saleha.moh2023@gmail.com>
2023-05-18 10:23:45 +0000

    add elif to check if the user enter no and else if the user enter anything other than yes or no

--- run.py
+++ run.py
@@ -1,25 +1,38 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def get_username_and_greet(name):
+    print("Welcome!")
+    print(f"Hello {username}, Lets play a game!")
+
 username = input("Please add your name: ")
-print(f"Hello {username}, Lets play a game!")
-user = input("Do you want to play game? yes/no \n").lower()
+
+get_username_and_greet(username)
+user = input("Do you want to play game?(yes/no) ")   
+
 secret_number = 6
 guess_num = 0
 guess_limit = 3
 if user == "yes":
     print("Game started")
     while guess_num < guess_limit:
-        guess = input("Guess a number: ")
-        guess_num += 1
+        try:
+            guess = int(input("Guess a number: "))
+            guess_num += 1
+        except ValueError:
+            print("Invalid, you are asked to enter a number")
+        
+
         if guess == secret_number:
             print(f"Hey {username} You won!")
             break
     else:
-        print("You Failed!")
+        print(f"{username} You Failed!")
+elif user == 'no':
+    print("You said no.")
 else:
-    print("start to start the game")
-
+    print("Sorry I do not undrestand that.\nYou need to write yes if you want to play and no if you do not want to play.")
         
 
 
